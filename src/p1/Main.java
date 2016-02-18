a2891925e30189f21d45d2c891b92f9bbbf24ff7
emailman <emailman@dtcc.edu>
2016-02-18 10:40:21 -0500

    Initial commit

---
+++ src/p1/Main.java
@@ -0,0 +1,56 @@
+package p1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pet dog1 = new Dog();
+        dog1.name = "Rover";
+        dog1.breed = "boxer";
+        dog1.weight = 20;
+        dog1.age = 1;
+
+        Pet cat1 = new Cat();
+
+        System.out.println(dog1);
+    }
+}
+
+class Pet {
+    String name;
+    String breed;
+    double weight;
+    int age;
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", weight=" + weight +
+                ", age=" + age +
+                '}';
+    }
+}
+
+class Dog extends Pet {
+    String vocalize = "bark";
+    int legs = 4;
+    Boolean houseTrained = false;
+
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "vocalize='" + vocalize + '\'' +
+                ", legs=" + legs +
+                ", houseTrained=" + houseTrained +
+                '}';
+    }
+}
+
+class Cat extends Pet {
+    String vocalize = "meow";
+    int legs = 4;
+    Boolean houseTrained = false;
+}
+
